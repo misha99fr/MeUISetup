cbeaa227b4387e8ae165161db8ee91745ab45c05
Michael Amudeshnikov <83592338+misha99fr@users.noreply.github.com>
2026-06-30 13:20:50 +0300

    sss

--- filelist.lua
+++ filelist.lua
@@ -1,15 +1,18 @@
 {
-	build = 20190406000001, --schematic:YEAR..MONTH..DAY..HOUR..MINUTE..VERSION
-	name = "1.0.0",
+	build = 20260630000001, --schematic:YEAR..MONTH..DAY..HOUR..MINUTE..VERSION
+	name = "1.1.0",
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/autorun.lua",path="/autorun.lua"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/OS.lua",path="/OS.lua"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/lib/crypt.lua",path="/lib/crypt.lua"},
+	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/lib/JSON.lua",path="/lib/JSON.lua"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/lib/TabletOSGraphics/init.lua",path="/lib/TabletOSGraphics/init.lua"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/lib/TabletOSGraphics/braile.lua",path="/lib/TabletOSGraphics/braile.lua"},
     	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/lib/TabletOSGraphics/QRCode.lua",path="/lib/TabletOSGraphics/QRCode.lua"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/lib/TabletOSCore.lua",path="/lib/TabletOSCore.lua"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/lib/TabletOSContextMenu.lua",path="/lib/TabletOSContextMenu.lua"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Service/Updater.lua",path="/TabletOS/Service/Updater.lua"},
+	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Service/updater-binary.lua",path="/TabletOS/Service/updater-binary.lua"},
+	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Recovery/Main.lua",path="/TabletOS/Recovery/Main.lua"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Lang/OS/eu_RU.lang",path="/TabletOS/Lang/OS/eu_RU.lang"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Lang/OS/eu_EN.lang",path="/TabletOS/Lang/OS/eu_EN.lang"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Lang/Settings/eu_RU.lang",path="/TabletOS/Lang/Settings/eu_RU.lang"},
@@ -23,6 +26,11 @@
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Apps/Settings.pkg/Modules/Security.lua",path="/TabletOS/Apps/Settings.pkg/Modules/Security.lua"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Apps/Settings.pkg/Modules/Language.lua",path="/TabletOS/Apps/Settings.pkg/Modules/Language.lua"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Apps/Settings.pkg/Modules/OTA.lua",path="/TabletOS/Apps/Settings.pkg/Modules/OTA.lua"},
+	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Apps/Settings.pkg/Modules/DeviceInfo.lua",path="/TabletOS/Apps/Settings.pkg/Modules/DeviceInfo.lua"},
+	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Apps/Settings.pkg/Modules/PowerSaving.lua",path="/TabletOS/Apps/Settings.pkg/Modules/PowerSaving.lua"},
+	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Apps/Settings.pkg/Modules/Notifications.lua",path="/TabletOS/Apps/Settings.pkg/Modules/Notifications.lua"},
+	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Apps/Settings.pkg/Modules/Developer.lua",path="/TabletOS/Apps/Settings.pkg/Modules/Developer.lua"},
+	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Apps/Settings.pkg/Modules/Recovery.lua",path="/TabletOS/Apps/Settings.pkg/Modules/Recovery.lua"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Guide/ContextMenuDoubleAction.lua",path="/TabletOS/Guide/ContextMenuDoubleAction.lua"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Guide/LowMemory.lua",path="/TabletOS/Guide/LowMemory.lua"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Guide/Start.lua",path="/TabletOS/Guide/Start.lua"},
@@ -30,6 +38,9 @@
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Apps/Settings.pkg/Modules/MiAccount.lua",path="/TabletOS/Apps/Settings.pkg/Modules/MiAccount.lua"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Apps/Settings.pkg/Modules/Bootloader.lua",path="/TabletOS/Apps/Settings.pkg/Modules/Bootloader.lua"},
 	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Apps/Settings.pkg/Modules/About.lua",path="/TabletOS/Apps/Settings.pkg/Modules/About.lua"},
+	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Apps/Notes.pkg/Main.lua",path="/TabletOS/Apps/Notes.pkg/Main.lua"},
+	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Apps/AeDA.pkg/Main.lua",path="/TabletOS/Apps/AeDA.pkg/Main.lua"},
+	{url="https://raw.githubusercontent.com/misha99fr/MeUI/Stable/TabletOS/Menu/%D0%97%D0%B0%D0%BC%D0%B5%D1%82%D0%BA%D0%B8.lnk",path="/TabletOS/Menu/Заметки.lnk"},
 	{
         url = "https://raw.githubusercontent.com/IgorTimofeev/AdvancedLua/master/AdvancedLua.lua",
         path = "/lib/advancedLua.lua"
